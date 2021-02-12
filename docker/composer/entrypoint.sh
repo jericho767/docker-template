559afe4c3ce22ce020c31833eb17d23d3636ade0
Jericho <jericho.6367@gmail.com>
2021-02-12 20:01:21 +0900

    Remove use of hirak/prestissimo.

--- docker/composer/entrypoint.sh
+++ docker/composer/entrypoint.sh
@@ -7,9 +7,5 @@ if [ "$1" = 'ash' ]; then
   exit $?
 fi
 
-if [[ ! -d $COMPOSER_HOME/vendor/hirak/prestissimo ]]; then
-  composer global require hirak/prestissimo
-fi
-
 composer --version
 exec php -d memory_limit=512M `which composer` "$@"
